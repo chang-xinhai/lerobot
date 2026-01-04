267e9960019d6e8f30960c91e5d7ed5f546e0c9d
walkerch <changxinhai@stu.pku.edu.cn>
2026-01-04 15:11:21 +0000

    Fix log checkpoint to wandb

--- src/lerobot/scripts/lerobot_train.py
+++ src/lerobot/scripts/lerobot_train.py
@@ -440,8 +440,9 @@ def train(cfg: TrainPipelineConfig, accelerator: Accelerator | None = None):
                     postprocessor=postprocessor,
                 )
                 update_last_checkpoint(checkpoint_dir)
-                if wandb_logger:
-                    wandb_logger.log_policy(checkpoint_dir)
+                # Optional: log checkpoint to wandb
+                # if wandb_logger:
+                #     wandb_logger.log_policy(checkpoint_dir)
 
             accelerator.wait_for_everyone()
 
