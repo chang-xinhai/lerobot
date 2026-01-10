a77827db16ef3fdf01ade3e378e118540cfce31b
Yaenday <changxinhai@stu.pku.edu.cn>
2026-01-10 09:55:19 +0800

    add dataset.preload

--- src/lerobot/configs/default.py
+++ src/lerobot/configs/default.py
@@ -35,6 +35,7 @@ class DatasetConfig:
     use_imagenet_stats: bool = True
     video_backend: str = field(default_factory=get_safe_default_codec)
     streaming: bool = False
+    preload: bool = False
 
 
 @dataclass
