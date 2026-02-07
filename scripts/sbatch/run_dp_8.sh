4d1787cef50b72f257e6eb50f7ab993836512171
Yaenday <changxinhai@stu.pku.edu.cn>
2026-02-07 16:47:23 +0800

    feat(sbatch): update run_dp_1.sh parameters and add run_dp_8.sh script for multi-GPU training

--- scripts/sbatch/run_dp_8.sh
+++ scripts/sbatch/run_dp_8.sh
@@ -1,27 +1,34 @@
 #! /bin/bash
 
 #SBATCH --partition=h100
-#SBATCH --job-name=dp_1
+#SBATCH --job-name=dp_8
 #SBATCH --ntasks=1
-#SBATCH --gres=gpu:1
-#SBATCH --cpus-per-task=50
-#SBATCH --mem=500G  
+#SBATCH --gres=gpu:8
+#SBATCH --cpus-per-task=100
+#SBATCH --mem=1000G  
 #SBATCH --time 5-00:00:00
 
 echo "Job started at $(date)"
 echo "Memory limit: $SLURM_MEM_PER_NODE"
 echo "CPU cores: $SLURM_CPUS_PER_TASK"
 
+export NCCL_TIMEOUT=10800
+export GLOO_TIMEOUT=10800
+
 exp_name="multi_object_open_7221_scene_0_seed_0"
 dataset_root=data/lerobot/$exp_name
 rm -rf outputs/train/dp_$exp_name
-lerobot-train \
+accelerate launch \
+  --multi_gpu \
+  --num_processes=8 \
+  --mixed_precision=fp16 \
+  $(which lerobot-train) \
   --policy.type=diffusion \
   --batch_size=512 \
-  --steps=100000 \
+  --steps=5000 \
   --log_freq=50 \
   --eval_freq=500 \
-  --save_freq=10000 \
+  --save_freq=1000 \
   --job_name=dp_$exp_name \
   --dataset.repo_id=$exp_name \
   --dataset.root=$dataset_root \
